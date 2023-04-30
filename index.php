ce349e5219cfffed71ac09785d7b5b95d6863d43
omarbessi04 <omarbessi04@gmail.com>
2023-04-30 17:49:05 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
-<html>
-    <body>
-        <h1> Welcome! Choose a dungeon!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mtgdungeons</title>
+</head>
+<body>
+    <?php
+    echo "dungeon";
+    ?>
+    <h1> Welcome! Choose a dungeon!</h1>
         
-        <img src="img/Dungeon_of_the_Mad_Mage.jpg">
-        <a href ="MadM/MadM.php">Dungeon of the Mad Mage</a>
-        <p>The long one.</p>
+    <img src="img/Dungeon_of_the_Mad_Mage.jpg">
+    <a href ="MadM/MadM.php">Dungeon of the Mad Mage</a>
+    <p>The long one.</p>
 
-        <img src="img/Lost_Mine_of_Phandelver.jpg">
-        <a href ="LostMine/LostMine.php">Lost Mine of Phandelver</a>
-        <p>Watch out for traps.</p>
+    <img src="img/Lost_Mine_of_Phandelver.jpg">
+    <a href ="LostMine/LostMine.php">Lost Mine of Phandelver</a>
+    <p>Watch out for traps.</p>
 
-        <img src="img/The_Undercity.png">
-        <a href ="TheUnder/TheUnder.php">The Undercity</a>
-        <p>Take the initiative!</p>
+    <img src="img/The_Undercity.png">
+    <a href ="TheUnder/TheUnder.php">The Undercity</a>
+    <p>Take the initiative!</p>
 
-        <img src="img/Tomb_of_Annihilation.jpg">
-        <a href ="TombAnn/TombAnn.php">Tomb of Annihilation</a>
-        <p>Summon the Atropal!</p>
-    </body>
+    <img src="img/Tomb_of_Annihilation.jpg">
+    <a href ="TombAnn/TombAnn.php">Tomb of Annihilation</a>
+    <p>Summon the Atropal!</p>
 </html>
